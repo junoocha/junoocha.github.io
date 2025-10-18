9a3beebe61d928bf8608dc57f5760fd22bdbbc49
[Caleb Cha] <junoocha@gmail.com>
2025-10-17 23:59:15 -0400

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // enables static export
-  basePath: "/your-repo-name", // replace with your GitHub repo name
-  images: {
-    unoptimized: true, // Next.js image optimization won't work on GitHub Pages
-  },
+  basePath: "/junoocha.github.io", // your repo name
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
